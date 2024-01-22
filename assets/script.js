f4712e4b45c012475c358d1275c70102f798c680
Dominic <dominicepaul@gmail.com>
2024-01-22 23:49:08 +0000

    created for loop to run through hours and check whether it is past, present or future

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,17 @@ var presentHour = moment().hour() - 9;
 var date = $("<h1>");
 date.text(moment().format("dddd MMMM Do" + ", " + "YYYY"));
 container.append(date);
+
+function pageLoad () {
+    for (i = 0; i < -9; i++) {
+        if (i < presentHour) {
+            var timeState = "past";
+        }
+        else if (i === presentHour) {
+            var timeState = "present";
+        }
+        else if (i > presentHour) {
+            var timeState = "future";
+        }
+    }
+}
